91812198d06f9ba7bc7436041558209dd277d179
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-05-07 12:26:03 +0545

    Arrays

---
+++ arrays_all/BasicSyntaxofArray.java
@@ -0,0 +1,18 @@
+package arrays_all;
+
+public class BasicSyntaxofArray {
+    public static void main(String[] args) {
+        //Syntax 1:
+        int[] arr=new int[5];
+
+        //Syntax 2:
+        int[] arr2;
+        arr2=new int[5];
+
+        //Syntax 3:
+        int[] arr3={4,3,1,6,3};
+
+        //Syntax 4:
+        int[] arr4=new int[] {1,2,3,4,5};
+    }
+}
